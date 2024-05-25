e781b71787f3c278b21c65868c495b477f3cf599
Dia Ganesh Kumar <dia.ganeshkumar@gmail.com>
2024-05-25 16:41:43 -0700

    dia's changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import json
 from flask import Flask, render_template, request, jsonify
+from melissa_api import verify_address
+import json
 
 app = Flask(__name__)
 
-# Load login credentials from a JSON file
 with open('login_data.json', 'r') as file:
     login_data = json.load(file)
 
@@ -11,12 +11,19 @@ with open('login_data.json', 'r') as file:
 def index():
     return render_template("index.html")
 
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
 @app.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email")
     print(email)
-
-    # Check if the provided email exists in the login data
+    
     if any(user["email"] == email for user in login_data):
         print('true')
         return jsonify({"success": True, "message": "Email validated"})
@@ -24,6 +31,20 @@ def login():
         print('false')
         return jsonify({"success": False, "message": "Email not validated"})
 
+@app.route("/verify-address", methods=["POST"])
+def verify_address_route():
+    data = request.get_json()
+    address = data.get("address")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if address:
+        is_valid = verify_address(address)
+        if is_valid:
+            return jsonify({"success": True, "message": "Address validated"})
+        else:
+            return jsonify({"success": False, "message": "Address not validated"})
+    else:
+        return jsonify({"error": "Address not provided"}), 400
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
