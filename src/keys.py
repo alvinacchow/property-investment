8dd0e21ffcb722957a6b24cff8df9b4bbd9e64de
Dia Ganesh Kumar <dia.ganeshkumar@gmail.com>
2024-05-25 16:42:35 -0700

    Merge branch 'main' of https://github.com/alvinacchow/property-investment

---
+++ src/keys.py
@@ -0,0 +1,5 @@
+class Key:
+    rentcast_key1 = "68ab1daf5b7b45c8af321a306597655f" # alvina's
+    rentcast_key2 = "8a28b974d0fb46cf950861487b51e120" # jenny's
+    rentcast_key3 = "MCyQtIXDDgCjb4K3S3qwcn**nSAcwXpxhQ0PC2lXxuDAZ-**" #dia's
+    #shruthi's key is ded :(
