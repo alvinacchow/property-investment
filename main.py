5690445eea9b74eed8d7ed500b23c805724f78df
Dia Ganesh Kumar <dia.ganeshkumar@gmail.com>
2024-05-25 17:13:54 -0700

    working frontend

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from melissa_api import verify_address
 import json
-<<<<<<< HEAD
-
-app = Flask(__name__)
-
-=======
 
 
 app = Flask(__name__)
 
-
->>>>>>> 6f72f6b2e904d9df25ef7f4295af238d9a25eb10
 with open('login_data.json', 'r') as file:
    login_data = json.load(file)
 
@@ -20,15 +13,9 @@ with open('login_data.json', 'r') as file:
 def index():
    return render_template("index.html")
 
-
 @app.route("/portfolio")
 def portfolio():
    return render_template("portfolio.html")
-
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
 
 @app.route("/search")
 def search():
@@ -36,7 +23,6 @@ def search():
 
 @app.route("/login", methods=["POST"])
 def login():
-<<<<<<< HEAD
     email = request.json.get("email")
     print(email)
     
@@ -46,35 +32,6 @@ def login():
     else:
         print('false')
         return jsonify({"success": False, "message": "Email not validated"})
-=======
-   email = request.json.get("email")
-   print(email)
-
-
-   # Check if the provided email exists in the login data
-   if any(user["email"] == email for user in login_data):
-       print('true')
-       return jsonify({"success": True, "message": "Email validated"})
-   else:
-       print('false')
-       return jsonify({"success": False, "message": "Email not validated"})
-
-
-@app.route("/verify-address", methods=["POST"])
-def verify_address_route():
-   data = request.get_json()
-   address = data.get("address")
-
-
-   if address:
-       is_valid = verify_address(address)
-       if is_valid:
-           return jsonify({"success": True, "message": "Address validated"})
-       else:
-           return jsonify({"success": False, "message": "Address not validated"})
-   else:
-       return jsonify({"error": "Address not provided"}), 400
->>>>>>> 6f72f6b2e904d9df25ef7f4295af238d9a25eb10
 
 @app.route("/verify-address", methods=["POST"])
 def verify_address_route():
@@ -92,9 +49,3 @@ def verify_address_route():
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-<<<<<<< HEAD
-=======
-if __name__ == '__main__':
-   app.run(debug = True)
->>>>>>> 6f72f6b2e904d9df25ef7f4295af238d9a25eb10
